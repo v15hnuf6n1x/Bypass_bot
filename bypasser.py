f0e55c0e9cb1d5ee0692efa0554b20e97b91e1c9
VISHNU <Vishnuvishva2434@gmail.com>
2024-06-15 13:38:02 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -248,7 +248,31 @@ def tnlink(url):
         return "Something went wrong :("
 
 
-###############################################################
+#############################################################################################################
+
+#\\\\\\\gtlinks\\\\\\\\\\
+async def gtlinks(url: str) -> str:
+    code = url.split('/')[-1]
+    useragent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
+    DOMAIN = "https://go.bloggingaro.com"
+    
+    async with ClientSession() as session:
+        async with session.get(f"{DOMAIN}/{code}", headers={'Referer':'https://tech.hipsonyc.com/','User-Agent': useragent}) as res:
+            cookies = res.cookies
+            html = await res.text()
+        async with session.get(f"{DOMAIN}/{code}", headers={'Referer':'https://hipsonyc.com/','User-Agent': useragent}, cookies=cookies) as resp:
+            html = await resp.text()
+        soup = BeautifulSoup(html, 'html.parser')
+        data = {inp.get('name'): inp.get('value') for inp in soup.find_all('input')}
+        await asleep(5)
+        async with session.post(f"{DOMAIN}/links/go", data=data, headers={'X-Requested-With':'XMLHttpRequest','User-Agent': useragent, 'Referer': f"{DOMAIN}/{code}"}, cookies=cookies) as links:
+            if 'application/json' in links.headers.get('Content-Type'):
+                json_data = await links.json()
+                try:
+                    json_data['url']
+                except:
+                   return "Something Went wrong"
+####################################################################################################
 # psa
 
 
