adc37500ed6319016da1f713b15bd9ba83c8a2d7
VISHNU <Vishnuvishva2434@gmail.com>
2024-06-15 17:25:13 +0530

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,23 @@
+from pyrogram import filters
+from pyrogram.enums import ChatMemberStatus
+from config import FORCE_SUB_CHANNEL, ADMINS
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+
+
+
+
+async def is_subscribed(filter, client, update):
+    if not FORCE_SUB_CHANNEL:
+        return True
+    user_id = update.from_user.id
+    if user_id in ADMINS:
+        return True
+    try:
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
+    except UserNotParticipant:
+        return False
+
+    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
+        return False
+    else:
+        return True
